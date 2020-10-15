e2f62e5a961f4de6872f4ffc3d4153c3a67c700b
Chris Troutner <chris.troutner@gmail.com>
2020-10-14 20:35:23 -0700

    Added two-token-paper-pays-fees.js e2e test

---
+++ test/e2e/two-token-paper-pays-fees.js
@@ -0,0 +1,93 @@
+/*
+  E2E test for sweeping a two token classes and paying for tx fees with the
+  paper wallet. e.g. the paper wallet has both a token and BCH on it.
+
+  Before running the test, this test will check that each wallet is set up correctly.
+  If they are not set up correctly, the test will exit and indicate what is wrong
+  with the set-up for the test.
+*/
+
+// These are the WIF (private keys) used to operate the test.
+const paperWif = 'KxtteuKQ2enad5jH2o5eGkSaTgas49kWmvADW6qqhLAURrxuUo7m'
+const receiverWif = 'L3nSksvTtHHBRP3HNMDhy6hDKpu88PQvrLGzLJn3FYX2diKqC1GD'
+
+// Unit under test
+const SweeperLib = require('../../index')
+
+async function runTest () {
+  try {
+    // Instancing the library
+    const sweeperLib = new SweeperLib(paperWif, receiverWif)
+    await sweeperLib.populateObjectFromNetwork()
+
+    await checkSetup(sweeperLib)
+
+    const hex = await sweeperLib.sweepTo2(sweeperLib.receiver.slpAddr)
+    // console.log(`hex: ${hex}`)
+
+    const txid = await sweeperLib.blockchain.broadcast(hex)
+
+    console.log('Transaction ID', txid)
+    console.log(`https://explorer.bitcoin.com/bch/tx/${txid}`)
+  } catch (error) {
+    console.error('Error in test: ', error)
+  }
+}
+runTest()
+
+// Check to ensure the test is set up correctly.
+async function checkSetup (sweeperLib) {
+  // console.log(
+  //   `receiving wallet BCH UTXOs: ${JSON.stringify(
+  //     sweeperLib.UTXOsFromPaperWallet.bchUTXOs,
+  //     null,
+  //     2
+  //   )}`
+  // )
+  // Ensure the Paper has a UTXO.
+  if (sweeperLib.UTXOsFromPaperWallet.bchUTXOs.length === 0) {
+    throw new Error(
+      `Paper wallet does not have BCH. Send 0.00005 BCH to ${
+        sweeperLib.paper.bchAddr
+      }`
+    )
+  }
+
+  // Ensure the Receiver has enough BCH to pay transaction fees.
+  console.log(`Paper wallet balance: ${sweeperLib.BCHBalanceFromPaperWallet}`)
+  if (sweeperLib.BCHBalanceFromPaperWallet < 5000) {
+    throw new Error(
+      'Paper wallet has less than 0.00005 BCH. Send that much to pay for transaction fees.'
+    )
+  }
+
+  // console.log(
+  //   `paper wallet SLP UTXOs: ${JSON.stringify(
+  //     sweeperLib.UTXOsFromPaperWallet.tokenUTXOs,
+  //     null,
+  //     2
+  //   )}`
+  // )
+  if (sweeperLib.UTXOsFromPaperWallet.tokenUTXOs.length === 0) {
+    throw new Error(
+      `Paper wallet does not have any tokens! Send some SLP tokens to ${
+        sweeperLib.paper.slpAddr
+      }`
+    )
+  }
+
+  // Get a list of token IDs contained in the paper wallet.
+  const tokenIds = sweeperLib.getTokenIds(
+    sweeperLib.UTXOsFromPaperWallet.tokenUTXOs
+  )
+  if (tokenIds.length < 2) {
+    throw new Error(
+      `Paper wallet has less than two token classes. Send more tokens to ${
+        sweeperLib.paper.slpAddr
+      }`
+    )
+  }
+
+  console.log('Tokens found on paper wallet. Test is good to go!')
+  console.log(' ')
+}
